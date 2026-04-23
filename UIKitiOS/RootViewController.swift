d517489ac33baa71f98149b5af68b04da0d3812c
Isabella Viksten <isabella.viksten@netlight.com>
2026-04-23 11:29:45 +0200

    Add button

--- UIKitiOS/RootViewController.swift
+++ UIKitiOS/RootViewController.swift
@@ -10,27 +10,50 @@ import UIKit
 // MARK: RootViewController
 final class RootViewController: UIViewController {
     private lazy var label: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.text = String(localized: .signupWelcomeLabel)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton.init(type: .roundedRect)
+        button.setTitle(String(localized: .signupButtonTitle), for: .normal)
+        button.addAction(UIAction{ [weak self] _ in self?.didTapSignupButton()}, for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [label, button])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
 }
 
 // MARK: Override
 extension RootViewController {
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		view.backgroundColor = .red
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        view.addSubview(stackView)
         
-        view.addSubview(label)
         NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            label.topAnchor.constraint(equalTo: view.topAnchor),
-            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
         
-	}
+    }
+}
+
+// MARK: Private
+private extension RootViewController {
+    private func didTapSignupButton() {
+        print("Tapped!")
+    }
 }
 
